702e4a9bc948d397ce8ac813ed5efea91be981ed
Jamesson Faria <jamessonjr@gmail.com>
2022-09-25 17:50:05 -0300

    consistencia em primeiro lugar

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,19 +1,23 @@
 /*
 *
 * Essa entidade é um dominio rico, contem regras de negocio
+* É necessario garantir a integridade dos dados, preciso ter todos os dados obrigatorios ao inicializar
+* o objeto precisa ser confiavel 100% das vezes
 *
 */
 
 class Customer {
     _id: string;
-    _name: string;
-    _address: string;
+    _name: string = "";
+    _address: string = "";
     _active: boolean = true;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string) {
         this._id = id;
-        this._name = name;
-        this._address = address;
+    }
+
+    set name(name: string) {
+        this._name  = name;
     }
 
     // motivo de negocio, nao apenas um set
@@ -30,6 +34,7 @@ class Customer {
     deactivate() {
         this._active = false;
     }
- 
-
 }
+
+// nesse o caso o cliente foi criado sem nome
+let customer = new Customer("123");
