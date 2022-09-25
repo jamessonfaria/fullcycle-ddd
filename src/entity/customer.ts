f2af3a9d5e72262b52b1445e30074ff5a0d17b71
Jamesson Faria <jamessonjr@gmail.com>
2022-09-25 18:03:57 -0300

    Auto validação

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,32 +1,44 @@
 /*
 *
 * Essa entidade é um dominio rico, contem regras de negocio
-* É necessario garantir a integridade dos dados, preciso ter todos os dados obrigatorios ao inicializar
+* 1- É necessario garantir a integridade dos dados, preciso ter todos os dados obrigatorios ao inicializar
 * o objeto precisa ser confiavel 100% das vezes
-*
+* 2- Uma entidade por padrao sempre tera que se autovalidar 
 */
 
 class Customer {
     _id: string;
-    _name: string = "";
+    _name: string;
     _address: string = "";
-    _active: boolean = true;
+    _active: boolean = false;
 
-    constructor(id: string) {
+    constructor(id: string, name: string) {
         this._id = id;
+        this._name = name;
+        this.validate();
     }
 
-    set name(name: string) {
-        this._name  = name;
+    validate() {
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
     }
 
     // motivo de negocio, nao apenas um set
     changeName(name: string) {
         this._name = name;
+        this.validate();
     }
 
     // expressa o negocio
     activate(){
+        if (this._address.length === 0) {
+            throw new Error("Address is mandatory to activate a customer");
+        }
+
         this._active = true;
     }
 
@@ -36,5 +48,5 @@ class Customer {
     }
 }
 
-// nesse o caso o cliente foi criado sem nome
-let customer = new Customer("123");
+// caso seja enviado o nome vazio a autovalidacao dara erro
+let customer = new Customer("123", "");
