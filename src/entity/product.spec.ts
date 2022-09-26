59e53c41599dfd299488f268394dc8c716b700b9
Jamesson Junior - Github <jamessonjr@gmail.com>
2022-09-26 15:35:53 -0300

    Implementação da classe Product + testes

---
+++ src/entity/product.spec.ts
@@ -0,0 +1,45 @@
+import Product from "./product";
+
+describe("Product unit tests", () => {
+
+    it("should throw error when id is empty", () => {
+
+       expect(() => {
+            let product = new Product("", "Product 1", 100);
+       }).toThrowError("Id is required");
+    
+    });
+
+    it("should throw error when name is empty", () => {
+
+        expect(() => {
+             let product = new Product("p1", "", 100);
+        }).toThrowError("Name is required");
+     
+     });
+
+     it("should throw error when price is less than zero", () => {
+
+        expect(() => {
+             let product = new Product("p1", "Product 1", -1);
+        }).toThrowError("Price must be greater than zero");
+     
+     });
+
+     it("should change name", () => {
+
+        let product = new Product("p1", "Product 1", 100);
+        product.changeName("Product 2");
+        expect(product.name).toBe("Product 2");
+     
+     });
+     
+     it("should change price", () => {
+
+        let product = new Product("p1", "Product 1", 100);
+        product.changePrice(150);
+        expect(product.price).toBe(150);
+     
+     });
+
+});
