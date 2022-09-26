e58c12c56f31305cf4bd456c0820c275e71fade7
Jamesson Junior - Github <jamessonjr@gmail.com>
2022-09-26 11:37:13 -0300

    Implementando testes para o Customer

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -1,8 +1,62 @@
+import Address from "./address";
+import Customer from "./customer";
+
 describe("Customer unit tests", () => {
 
-    it("should get 1 as result", () => {
-        const result = 1;
-        expect(result).toBe(1);
+    it("should throw error when id is empty", () => {
+        expect(() => {
+            let customer = new Customer("", "John");
+        }).toThrowError("Id is required");
+    });
+
+    it("should throw error when name is empty", () => {
+        expect(() => {
+            let customer = new Customer("132", "");
+        }).toThrowError("Name is required");
+    });
+
+    it("should change name", () => {
+        // Arrange
+        const customer = new Customer("123", "John");
+        
+        // Act
+        customer.changeName("Jane");
+        
+        // Assert
+        expect(customer.name).toBe("Jane");       
+    });
+
+    it("should activate customer", () => {
+        // Arrange
+        const customer = new Customer("1", "Customer 1");
+        const address = new Address("Street 1", 123, "00000-000", "Sao Paulo");
+        customer.Address = address;
+
+        // Act
+        customer.activate();
+        
+        // Assert
+        expect(customer.isActive()).toBe(true);       
+    });
+
+    it("should deactivate customer", () => {
+        // Arrange
+        const customer = new Customer("1", "Customer 1");
+
+        // Act
+        customer.deactivate();
+        
+        // Assert
+        expect(customer.isActive()).toBe(false);       
+    });
+
+    it("should throw erro when address is undefined when you activate a customer", () => {
+
+        expect(() => {
+            const customer = new Customer("1", "Customer 1");
+            customer.activate();
+        }).toThrowError("Address is mandatory to activate a customer")
+         
     });
 
 });
--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -11,10 +11,10 @@ import Address from './address';
 */
 
 export default class Customer {
-    _id: string;
-    _name: string;
-    _address!: Address;
-    _active: boolean = false;
+    private _id: string;
+    private _name: string;
+    private _address!: Address;
+    private _active: boolean = false;
 
     constructor(id: string, name: string) {
         this._id = id;
@@ -22,12 +22,16 @@ export default class Customer {
         this.validate();
     }
 
+    get name(): string {
+        return this._name;
+    }
+
     validate() {
-        if (this._name.length === 0) {
-            throw new Error("Name is required");
-        }
         if (this._id.length === 0) {
             throw new Error("Id is required");
+        }
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
         }
     }
 
@@ -35,6 +39,10 @@ export default class Customer {
     changeName(name: string) {
         this._name = name;
         this.validate();
+    }
+
+    isActive(): boolean {
+        return this._active;
     }
 
     // expressa o negocio
