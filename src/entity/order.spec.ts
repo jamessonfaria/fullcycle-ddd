f7ea95cb4509ce9beaffdd618daed3a138004e76
Jamesson Junior - Github <jamessonjr@gmail.com>
2022-09-26 12:02:27 -0300

    Implementando testes para o Order

---
+++ src/entity/order.spec.ts
@@ -0,0 +1,47 @@
+import Order from "./order";
+import OrderItem from "./order_item";
+
+describe("Order unit tests", () => {
+
+    it("should throw error when id is empty", () => {
+
+       expect(() => {
+            let order = new Order("", "123", []);
+       }).toThrowError("Id is required");
+    
+    });
+
+    it("should throw error when customerId is empty", () => {
+
+        expect(() => {
+             let order = new Order("123", "", []);
+        }).toThrowError("CustomerId is required");
+     
+     });
+
+     it("should throw error when customerId is empty", () => {
+
+        expect(() => {
+             let order = new Order("123", "1", []);
+        }).toThrowError("Items are required");
+     
+     });
+
+     it("should calculate total", () => {
+
+        const item1 = new OrderItem("i1", "Item 1", 100.0);
+        const item2 = new OrderItem("i2", "Item 2", 200.0);
+        const order = new Order("o1", "c1", [item1]);
+
+        const total = order.total();
+        expect(total).toBe(100);
+
+        const order2 = new Order("o1", "c1", [item1, item2]);
+        const total2 = order2.total();
+        expect(total2).toBe(300);
+     
+     });
+
+    
+
+});
--- src/entity/order.ts
+++ src/entity/order.ts
@@ -1,20 +1,37 @@
 import OrderItem from './order_item';
 
 export default class Order {
-    _id: string;
+    private _id: string;
     // se esta em diferentes agregado a relacao é por id
-    _customerId: string;
+    private _customerId: string;
     // se esta em mesmo agregado a relacao é pelo objeto
-    _items: OrderItem[] = [];
+    private _items: OrderItem[] = [];
+    private _total: number;
 
     constructor(id: string, customerId: string, items: OrderItem[]) {
         this._id = id;
         this._customerId = customerId;
         this._items = items;
+        this._total = this.total();
+        this.validate();
     }
 
     total(): number {
         return this._items.reduce((acc, item) => acc + item._price, 0);
+    }
+
+    validate(): boolean {
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
+        if (this._customerId.length === 0) {
+            throw new Error("CustomerId is required");
+        }
+        if (this._items.length === 0) {
+            throw new Error("Items are required");
+        }
+
+        return true;
     }
 
 }
