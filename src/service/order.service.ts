27e0753d36cf3f754a179acd46de4d2024c3004f
Jamesson Junior - Github <jamessonjr@gmail.com>
2022-09-30 18:52:38 -0300

    Implementando order service

---
+++ src/service/order.service.ts
@@ -0,0 +1,9 @@
+import Order from "../entity/order";
+
+export default class OrderService {
+
+    static total(orders: Order[]): number{
+        return orders.reduce((acc, order) => acc + order.total(), 0);
+    }
+
+}
