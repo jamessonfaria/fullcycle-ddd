b004e24f48a73db47be0cb114373a17c6b9fc9d9
Jamesson Junior - Github <jamessonjr@gmail.com>
2022-09-30 19:18:52 -0300

    Implementar teste para rewards points no customer

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -59,4 +59,15 @@ describe("Customer unit tests", () => {
          
     });
 
+    it("should add reward points", () => {
+        const customer = new Customer("1", "Customer 1");
+        expect(customer.rewardPoints).toBe(0);
+
+        customer.addRewardPoints(10);
+        expect(customer.rewardPoints).toBe(10);
+
+        customer.addRewardPoints(10);
+        expect(customer.rewardPoints).toBe(20);
+    });
+
 });
