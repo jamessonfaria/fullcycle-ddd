eb658a2d462f8a8a6be6f665672239e958014a82
Jamesson Junior - Github <jamessonjr@gmail.com>
2022-10-04 18:13:16 -0300

    Implementação do ProductRepository e o seu teste unitário

--- src/infrastructure/repository/product-repository.spec.ts
+++ src/infrastructure/repository/product-repository.spec.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
+import Product from "../../domain/entity/product";
 import ProductModel from "../db/sequelize/model/product.model";
+import ProductRepository from "./product.repository";
 
 describe("Product repository test", () => {
 
@@ -19,6 +21,68 @@ describe("Product repository test", () => {
 
     afterEach(async () => {
         await sequelize.close();
+    });
+
+    it("should create a product", async () => {
+        const productRepository = new ProductRepository();
+        const product = new Product("1", "Product 1", 100);
+        await productRepository.create(product);
+
+        const productModel = await ProductModel.findOne({ where: { id: "1" } });
+        expect(productModel.toJSON()).toStrictEqual({
+            id: "1", 
+            name: "Product 1", 
+            price: 100,
+        });
+    });
+
+    it("should update a product", async () => {
+        const productRepository = new ProductRepository();
+        const product = new Product("1", "Product 1", 100);
+        await productRepository.create(product);
+
+        product.changeName("Product 2");
+        product.changePrice(200);
+        await productRepository.update(product);
+
+        const productModel = await ProductModel.findOne({ where: { id: "1" } });
+        expect(productModel.toJSON()).toStrictEqual({
+            id: "1", 
+            name: "Product 2", 
+            price: 200,
+        });
+    });
+
+
+    it("should find a product", async () => {
+        const productRepository = new ProductRepository();
+        const product = new Product("1", "Product 1", 100);
+        await productRepository.create(product);
+
+        const productModel = await ProductModel.findOne({ where: { id: "1" } });
+
+        const foundProduct = await productRepository.find("1");
+
+        expect(productModel.toJSON()).toStrictEqual({
+            id: foundProduct.id,
+            name: foundProduct.name,
+            price: foundProduct.price
+        });
+    });
+
+    it("should find all products", async () => {
+        const productRepository = new ProductRepository();
+
+        const product1 = new Product("1", "Product 1", 100);
+        await productRepository.create(product1);
+
+        const product2 = new Product("2", "Product 2", 200);
+        await productRepository.create(product2);
+
+        const foundProducts = await productRepository.findAll();
+        const products = [product1, product2];
+
+        expect(products).toEqual(foundProducts);
     });
 
 });
