ab568a2a01ee19b222e9e45702af17c354dfa36e
Jamesson Junior - Github <jamessonjr@gmail.com>
2022-10-24 17:32:33 -0300

    Modelando order e items

---
+++ src/infrastructure/repository/order-repository.spec.ts
@@ -0,0 +1,30 @@
+import { Sequelize } from "sequelize-typescript";
+import Address from "../../domain/entity/address";
+import Customer from "../../domain/entity/customer";
+import CustomerModel from "../db/sequelize/model/customer.model";
+import CustomerRepository from "./customer.repository";
+
+
+describe("Order repository test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([CustomerModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  
+
+
+});
